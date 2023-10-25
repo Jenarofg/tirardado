57aead7ca2b6bff6e66828670a4a0571282fc4e5
jenarofg <jenarofg@gmail.com>
2023-10-25 11:55:35 +0200

    VersionFinal

--- app/src/main/java/com/jenaro/pmdm/helloworld/MainActivity.kt
+++ app/src/main/java/com/jenaro/pmdm/helloworld/MainActivity.kt
@@ -3,6 +3,7 @@ package com.jenaro.pmdm.helloworld
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class  MainActivity : AppCompatActivity() {
@@ -14,17 +15,46 @@ class  MainActivity : AppCompatActivity() {
         btntirar.setOnClickListener {
             tirarDado()
         }
+        /**
+         * Llamamos al método tirarDado para inicializarlo y que
+         * no salga la pantalla en blanco en el arranque.
+         */
+
+        tirarDado()
     }
 
+    /**
+     * Tira el dado y actualiza la imagen con el resultado
+     */
     private fun tirarDado() {
-        val txtDado = findViewById<TextView>(R.id.txt_dado)
+        //Crea un dado con 6 caras y tira el dadod
         val dado= Dado(6)
         val valorDado= dado.tirar()
-        txtDado.text= valorDado.toString()
+        //Busca la ImageView correspondiente.
+        val diceImage: ImageView = findViewById(R.id.imgDado)
+        //Determina que imagen usar en funcion del resultado obtenido en valorDado
+       val drawableResource = when (valorDado) {
+           1 -> R.drawable.dice_1
+           2 -> R.drawable.dice_2
+           3 -> R.drawable.dice_3
+           4 -> R.drawable.dice_4
+           5 -> R.drawable.dice_5
+           else -> R.drawable.dice_6
+        }
+        //Actualiza le ImageView con su imagen correspondiente
+        diceImage.setImageResource(drawableResource)
+        //Actualiza el contenido de la Descripcion con el valor numerico correspondiente
+        diceImage.contentDescription = diceImage.toString()
+
+        //txtDado.text= valorDado.toString()
     }
 }
 
 class Dado(val numCaras:Int){
+    /**
+     * Creamos la clase Dado , donde tenemos un método
+     * tirar(), que devolcerá un número aleatorio de 1 a numCaras.
+     */
     fun tirar():Int{
         return (1..numCaras ).random()
     }
